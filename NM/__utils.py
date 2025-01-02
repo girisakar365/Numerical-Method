cad63e26efcff1dddeb3c9e2a92e750e3dddac61
Sakar Giri <sakargiri365@gmail.com>
2025-01-02 19:17:51 +0545

    fonts

--- NM/__utils.py
+++ NM/__utils.py
@@ -1,11 +1,15 @@
 import yaml
+import json
 
 class Config:
     def __init__(self):
         
         self.__config_file = self.__load_config()
-        self.config_window = self.__config_file['window']
-        self.config_color = self.__config_file['color']
+        self.screen = self.__config_file['screen']
+        self.s_width, self.s_height = self.screen['size']
+        self.color = self.__config_file['color']
+        self.font = self.__config_file['font']
+        self.w_scale,self.h_scale = self.font['scale']
 
     def __load_config(self):
         yaml.add_constructor("!tup", lambda loader, node: tuple(loader.construct_sequence(node)))
